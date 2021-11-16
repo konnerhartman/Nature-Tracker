cd27eaa2a0fb7c9b03211549a657486876be54ce
Corbin Ball <c.ball@me.com>
2021-11-15 19:56:13 -0600

    updater user to find name and not email

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -58,12 +58,13 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const userData = await User.findOne({ where: { name: req.body.name } });
+    console.log = userData;
 
     if (!userData) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: 'Incorrect name or password, please try again' });
       return;
     }
 
@@ -72,7 +73,7 @@ router.post('/login', async (req, res) => {
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: 'Incorrect name or password, please try again' });
       return;
     }
 
