86b74e8140a5ea277f65033f642dad93ebc53c4b
Corbin Ball <c.ball@me.com>
2021-11-15 19:00:00 -0600

    linked login js and login handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -2,10 +2,10 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
   
     // Collect values from the login form
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const name = document.querySelector('#floatingInput').value.trim();
+    const password = document.querySelector('#floatingPassword').value.trim();
   
-    if (email && password) {
+    if (name && password) {
       // Send a POST request to the API endpoint
       const response = await fetch('/api/users/login', {
         method: 'POST',
@@ -25,14 +25,14 @@ const loginFormHandler = async (event) => {
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const name = document.querySelector('#floatingInput').value.trim();
+    //const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#floatingPassword').value.trim();
   
-    if (name && email && password) {
+    if (name && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ name, email, password }),
+        body: JSON.stringify({ name, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
