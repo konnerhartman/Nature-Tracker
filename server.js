9a10ddcfdc6c65d507a3a3f30a603f4d7ed1ad34
Wanda M <wanda.avery@att.net>
2021-11-17 19:18:49 -0600

    added modules for as dev dependencies and implemented global variable for tracking user (logged-in).

--- server.js
+++ server.js
@@ -5,11 +5,18 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-
+const morgan = require('morgan');
+const methodOverride = require('method-override');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
+
+// only run in development
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'));
+}
+
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
@@ -31,6 +38,12 @@ app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// Set global var
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -38,5 +51,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening and Server running
+  in ${process.env.NODE_ENV} on port ${PORT}`));
 });
